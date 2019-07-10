bc86d60558094610f35e1615a3fff2ef6756f0b8
Mateaus <mateausc@gmail.com>
2019-07-09 21:35:11 -0500

    Refactored TagFooterViewHolder and its layout.

--- app/src/main/java/com/example/mat/roomdb_mvvm/mainactivity/adapter/viewholder/TagFooterViewHolder.java
+++ app/src/main/java/com/example/mat/roomdb_mvvm/mainactivity/adapter/viewholder/TagFooterViewHolder.java
@@ -1,38 +1,36 @@
 package com.example.mat.roomdb_mvvm.mainactivity.adapter.viewholder;
 
 import android.view.View;
-import android.widget.Button;
 
-import com.example.mat.roomdb_mvvm.R;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.mat.roomdb_mvvm.databinding.TagFooterItemBinding;
 import com.example.mat.roomdb_mvvm.expandablerecyclerview.viewholders.ChildViewHolder;
 import com.example.mat.roomdb_mvvm.mainactivity.entity.TagCategory;
 import com.example.mat.roomdb_mvvm.mainactivity.listener.OnNewTagClickListener;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TagFooterViewHolder extends ChildViewHolder {
 
-    @BindView(R.id.footerB)
-    Button footerB;
-
-    private View view;
+    private TagFooterItemBinding viewBinding;
 
     public TagFooterViewHolder(View itemView) {
         super(itemView);
+        viewBinding = DataBindingUtil.bind(itemView);
         ButterKnife.bind(this, itemView);
-        this.view = itemView;
     }
 
     public void setFooter(String footer, int color) {
-        footerB.setText(footer);
-        footerB.setBackgroundTintList(view.getContext().getResources().getColorStateList(color));
+        viewBinding.tagFooterItemBtn.setText(footer);
+        viewBinding.tagFooterItemBtn.setBackgroundTintList(
+                viewBinding.getRoot().getContext().getResources().getColorStateList(color));
     }
 
     public void setNewTagClickListener(final TagCategory tagCategory,
                                        final OnNewTagClickListener onNewTagClickListener) {
 
-        footerB.setOnClickListener(new View.OnClickListener() {
+        viewBinding.tagFooterItemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onNewTagClickListener.onNewTagClick(tagCategory);
